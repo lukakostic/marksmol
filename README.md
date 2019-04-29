17ffab04052f2de17026dc2f0f4cd02a8e89395d
Luka Kostić <41348897+lukakostic@users.noreply.github.com>
2019-04-29 07:26:57 +0200

    start args, templates

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-My small and simplified version of HTML, called marksmol
+## My small and simplified version of HTML, called marksmol
+
+Still need to add includes, multiple language versions, options and a visual studio code extension
 
 
 ```html
@@ -139,4 +141,64 @@ a<`Some Text`
 '<' adds newline + tab
 ```
 
-Still need to add templates, multiple language versions, options and a visual studio code extension
+## Templates/Functions
+
+```
+$hi $person $action:
+`\`Hello $person, nice to $action you!\``
+
+test
+	$hi `Luka` `meet`:
+	$hi `Bob` `see`:
+```
+results in:
+```
+<test>
+	Hello Luka, nice to meet you!
+	Hello Bob, nice to see you!
+</test>
+```
+
+
+```
+Quotes escaped so  the pased text would be:
+`Hello $person, nice to $action you!`
+if without then its not considered text:
+Hello $person, nice to $action you!
+and 'Hello' would become <Hello></Hello>
+```
+
+':' signifies an end of an function, if it was already declared then it pastes the content.
+
+function names must start with '$' but its variables (eg '$person') dont. Tho since it does a simple replace, its recomended to make them unique (eg dont make it 'a' as then it will replace all a's in the text. '$a' is better.)
+
+You can make nested functions:
+
+```
+$hi $person $action:
+`$post \`Hi $person, nice to $action you!\`:`
+
+$post $text:
+`post
+	a
+		\`$text\``
+
+test
+	$hi `Luka` `meet`:
+	$hi `Bob` `see`:
+```
+results in:
+```
+<test>
+	<post>
+		<a>
+			Hi Luka, nice to meet you!
+		</a>
+	</post>
+	<post>
+		<a>
+			Hi Bob, nice to see you!
+		</a>
+	</post>
+</test>
+```
--- marksmol.py
+++ marksmol.py
@@ -1,24 +1,45 @@
+import sys
 import os
 import pathlib
 
-def parse(t):
+
+def parse(t,debug = False):
+    #Current strings
     code = ''
     word = ''
     line = ''
+    
+    #State trackers
     inQuotes = 0
-    inComment = False
-    indentation = 0
+    inComment = 0 #not bool so it can have comments in comments
     escapeNext = False
+
+    #Indentation/Parent tracking
+    indentation = 0
     tagStack = []
     rootIndentation = -1
 
-    t = t.replace(' '*4, '\t').replace("\r\n",'\n')
+    #Functions/Templates stuff
+    inFunc = False
+    funcFirst = True
+    textNext = False
+    funcs = []
 
-        #DEBUG
+    t = t.replace(' '*4, '\t').replace("\r\n",'\n') #Replace 4 spaces with tabs, replace \r\n with \n
+
+    class marksmolFunc:
+        def __init__(self,name,variables,text):
+            self.name = name
+            self.variables = variables
+            self.text = text
+
+    #DEBUG
     def dprint(txt):
-        if False:
+        nonlocal debug
+        if debug:
             print(txt)
 
+    #Get second to last tag (pop it)
     def getPrevTag():
         nonlocal tagStack
 
@@ -29,33 +50,94 @@ def parse(t):
             return retTag
         return tag
 
+    def funcEnd(l):
+        nonlocal code, word, tagStack, line, indentation, inFunc, funcFirst, funcs, textNext, t
+        
+        fnInd = -1
+
+        for x in range(len(funcs)):
+            #dprint('search ['+str(x)+'] name: |' + funcs[x].name + '| vs |' + funcs[-1].name + '|')
+            if funcs[x].name == funcs[-1].name:
+                fnInd = x
+                break
+                
+
+        dprint('fnInd : ' + str(fnInd))
+        
+        if fnInd is (len(funcs)-1) or fnInd is -1:
+            #Is new
+            dprint('fn is new')
+
+            textNext = True
+        else:
+            #Do function stuff
+            dprint('fn is old')
+            
+            textToPaste = funcs[fnInd].text.replace('\r\n','\n')
+
+            textToPaste = '\t'*indentation + textToPaste.replace('\n','\n'+'\t'*indentation) #indent
+
+            #dprint('FUNCTION TEXT:\n' + textToPaste)
+
+            #replace var names with var values
+            for i in range(len(funcs[-1].variables)):
+                textToPaste = textToPaste.replace(funcs[fnInd].variables[i],funcs[-1].variables[i][1:])
+
+            t = t[:l+1] + '\n' + textToPaste + t[l+1:]
+            
+            #dprint('FUNCTION TEXT RESULT:\n' + textToPaste)
+
+            #dprint('FUNCTION RESULT:\n' + t)
+
+            funcs.pop()
+
+            inFunc = False
+            textNext = False
+            funcFirst = True
+
     def endWord():
-        nonlocal code, word, tagStack, line, indentation
+        nonlocal code, word, tagStack, line, indentation, inFunc, funcFirst, funcs, textNext
         
         if word is not '' and word.isspace() is False and indentation is not -2:
-            dprint('nw='+word)
+            if not inFunc:
+                dprint('nw='+word)
 
-            lineEmpty = line.isspace() or line is ''
+                lineEmpty = line.isspace() or line is ''
 
-            if lineEmpty:
-                tagStack.append(word)
-                dprint(str(indentation)+'tag+'+word)
-            
-            if not lineEmpty:
-                line = line.rstrip(' ')
-                line += ' '
-            elif not word.startswith('`'):
-                line += '<'
-            
-            line += word
-
+                if lineEmpty:
+                    tagStack.append(word)
+                    dprint(str(indentation)+'tag+'+word)
+                
+                if not lineEmpty:
+                    line = line.rstrip(' ')
+                    line += ' '
+                elif not word.startswith('`'):
+                    line += '<'
+                
+                line += word
+            else:
+                dprint('inFunc : ' + word)
+                if funcFirst:
+                    dprint('funcName')
+                    funcs.append(marksmolFunc(word,[],''))
+                    funcFirst = False
+                else:
+                    if textNext:
+                        dprint('funcText')
+                        funcs[-1].text = word[1:]
+                        inFunc = False
+                        textNext = False
+                        funcFirst = True
+                    else:
+                        dprint('funcVar')
+                        funcs[-1].variables.append(word)
         word = ''
 
-            
-
-    
     def endLine():
-        nonlocal code, word, tagStack, indentation, rootIndentation, line
+        nonlocal code, word, tagStack, indentation, rootIndentation, line, inFunc
+
+        if inFunc:
+            return
 
         if line.isspace() or line is '':
             indentation = 0
@@ -100,15 +182,19 @@ def parse(t):
             rootIndentation = indentation
         indentation = 0
 
-        
-
-    for l in range(len(t)):
+    l = 0
+    while l < len(t):
+        #dprint('l:'+t[l])
         if escapeNext:
             word += t[l]
             escapeNext = False
-        elif inComment:
+        elif inComment > 0:
             if t[l] is '}':
-                inComment = False
+                inComment -= 1
+                dprint('}')
+            elif t[l] is '{':
+                inComment += 1
+                dprint('{')
         elif inQuotes > 0:
             if t[l] is '"' and inQuotes is 1:
                 word+='"'
@@ -122,6 +208,7 @@ def parse(t):
                 #word+='`'
                 endWord()
                 inQuotes = 0
+                textNext = False
             elif t[l] is '\n' and inQuotes is 4:
                 #word+='`'
                 endWord()
@@ -138,6 +225,16 @@ def parse(t):
             elif t[l] is '\n':
                 endWord()
                 endLine()
+            elif t[l] is '$' and word is '':
+                inFunc = True
+                word = '$'
+                dprint('$')
+            elif t[l] is ',' and inFunc:
+                endWord()
+            elif t[l] is ':' and inFunc:
+                endWord()
+                dprint(':')
+                funcEnd(l)
             elif t[l] is ';':
                 endWord()
                 preInd = indentation
@@ -167,7 +264,8 @@ def parse(t):
             elif t[l] is ' ':
                 endWord()
             elif t[l] is '{':
-                inComment = True
+                inComment += 1
+                dprint('{')
             elif t[l] is '"':
                 word='"'
                 inQuotes = 1
@@ -182,6 +280,7 @@ def parse(t):
                 escapeNext = True
             else:
                 word += t[l]
+        l += 1
     
     endWord()
     endLine()
@@ -193,8 +292,25 @@ def parse(t):
     return code
 
 def main():
-    folder = input("Folder/Root:")
-    rec = input("Recursive (y/n):")[0].lower()=="y"
+    folder = ''
+    rec = ''
+    debug = False
+
+    for x in range(len(sys.argv)):
+        if sys.argv[x] == '--folder':
+            folder = sys.argv[x+1]
+        if sys.argv[x] == '--rec':
+            rec = sys.argv[x+1]
+        if sys.argv[x] == '--debug':
+            debug = sys.argv[x+1]
+    
+    if folder == '':
+        folder = input("Folder/Root:")
+    if rec == '':
+        rec = input("Recursive (y/n):")
+
+    rec = rec[0].lower()=="y"
+    debug = debug[0].lower()=="y"
 
     files = []
     if rec is False:
@@ -206,7 +322,7 @@ def main():
     for f in files:
         txt = pathlib.Path(f).read_text()
         file = open(os.path.splitext(f)[0]+'.html','w')
-        file.write(parse(txt))
+        file.write(parse(txt,debug))
         print(file.name)
         file.close()
 
